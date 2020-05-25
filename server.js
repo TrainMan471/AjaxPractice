c4cbf77c90142d8ae25b4d0b31e327c62fd7b12a
Michael Brown <brownmichael486@gmail.com>
2020-05-24 23:54:52 -0400

    Server now serves html

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const http = require('http');
+let http = require('http');
+let fs = require('fs');
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+let server = http.createServer(function (req, res) {
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  const myReadStream = fs.createReadStream(__dirname + '/index.html/', 'utf8');
+  myReadStream.pipe(res);//shoving the above line of code into the variable 'res';
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("Success");
