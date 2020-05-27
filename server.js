147a7a328ebcc89a83001d7c2cbc170c57c1cd94
Michael Brown <brownmichael486@gmail.com>
2020-05-26 21:51:36 -0400

    fixed server issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 let server = http.createServer(function (req, res) {
   console.log('request was made: ' + req.url);
   res.writeHead(200, {'Content-Type': 'text/html'});
-  const myReadStream = fs.createReadStream(__dirname + '/index.html/', 'utf8');
+  const myReadStream = fs.createReadStream(__dirname + '/'+req.url+'/', 'utf8');//load url request 
   myReadStream.pipe(res);//shoving the above line of code into the variable 'res';
 
 });
